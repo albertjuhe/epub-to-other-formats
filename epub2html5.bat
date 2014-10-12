df8eb3f3783a0f71e2732760230ef6c8182ff98a
Albert <albert@omaonk.com>
2014-10-12 20:26:05 +0200

    epub v1

--- epub2html5.bat
+++ epub2html5.bat
@@ -6,28 +6,30 @@ REM    V.1.0
 REM ***************************************************
 cls
 set Code=%1
-set origin=..\epub2html
+set origin=.
 set Saxon_path=..\SaxonHE9-4-0-1J\saxon9he.jar
-set tools=%origin%\tools
-set input_process=%origin%\input\
-set output_process=%origin%\output\
-set tmp=%origin%\tmp\
-set xsl=%origin%\xsl\
-IF NOT EXIST %input_process%%Code% GOTO ERROR_FILE
+set tools=%origin%\tools\
+set input_process=%origin%\epub_input\
+set output_process=%origin%\output\%Code%
+set tmp=%origin%\tmp\%Code%
+set xsl=%origin%\xsl\HTML5\
+IF NOT EXIST %input_process%%Code%.epub GOTO ERROR_FILE
 IF NOT EXIST %Saxon_path% GOTO ERROR_SAXON
 
-echo Opening document %Code%
-%tools%unzip.exe -d %tmp% %input_process%%Code%
+echo Opening document %Code%.epub
+md %tmp%
+md %output_process%
+%tools%unzip.exe -d %tmp% %input_process%%Code%.epub
 echo Transforming document to HTML5
-
-java -jar %Saxon_path% %tmp%/META-INF/container.xml -o %output_process%/output.html
+pause
+java -jar %Saxon_path%  %xsl%main.xsl %tmp%/%Code%/META-INF/container.xml -o %output_process%/output.log
 
 PAUSE
-rem copy %tmp%word\media\*.* %output_process%
+copy %tmp%word\media\*.* %output_process%
 GOTO FINAL
 
 :ERROR_FILE
-echo [ERROR]: Doesn't exists the source file: %input_process%%Code%
+echo [ERROR]: Doesn't exists the source file: %input_process%%Code%.epub
 echo OUTPUT ERROR.
 GOTO THEEND
 
